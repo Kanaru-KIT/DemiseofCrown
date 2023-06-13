2301b601e351bc6e3942e489308f18efdd58bcb5
Kanaru-KIT <112527552+Kanaru-KIT@users.noreply.github.com>
2023-06-13 15:13:39 +0900

    Add files via upload
    Signed-off-by: Kanaru-KIT <112527552+Kanaru-KIT@users.noreply.github.com>

---
+++ Animation/ProcessEnemyAnimEvent.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+
+public class ProcessEnemyAnimEvent : MonoBehaviour
+{ 
+
+    private Enemy enemy;
+    [SerializeField]
+    private SphereCollider sphereCollider;
+
+    void Start()
+    {
+        enemy = GetComponent<Enemy>();
+    }
+
+    void AttackStart()
+    {
+        sphereCollider.enabled = true;
+    }
+
+    public void AttackEnd()
+    {
+        if (sphereCollider != null)
+        {
+            sphereCollider.enabled = false;
+        }
+    }
+
+    public void StateEnd()
+    {
+        enemy.SetState(Enemy.EnemyState.Freeze);
+    }
+
+    public void EndDamage()
+    {
+        enemy.SetState(Enemy.EnemyState.Walk);
+    }
+}
